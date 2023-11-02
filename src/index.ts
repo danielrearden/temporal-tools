4bac0e79a9b9f323c10003f8bc56e8e6ab670680
Daniel Rearden <danielrearden@gmail.com>
2023-11-01 21:39:23 -0400

    fix: export serializers

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,11 @@ export { createWorkerHelpers } from "./worker.js";
 export { createWorkflowHelpers } from "./workflow.js";
 export { createClient } from "./client.js";
 export { createListFilterQueryBuilder } from "./queryBuilder.js";
-export { createCustomDataConverterSerializer, CustomPayloadConverter } from "./payloadConverter.js";
+export {
+  createCustomDataConverterSerializer,
+  CustomPayloadConverter,
+  BigIntSerializer,
+  DateSerializer,
+  RegExpSerializer,
+  URLSerializer,
+} from "./payloadConverter.js";
